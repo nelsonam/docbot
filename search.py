c7bfa4962eae212e71e8c39689a3b0b1f7fb1162
Allison Nelson <allison.nelson13@gmail.com>
2016-02-01 15:59:02 -0800

    add search.py file, bot coming soon

---
+++ search.py
@@ -0,0 +1,24 @@
+import sys
+import urllib
+import requests
+from bs4 import BeautifulSoup
+
+query = sys.argv[1]  # the query
+alpine = "https://alpine.atlassian.net"
+base_url = "https://alpine.atlassian.net/wiki/dosearchsite.action?queryString="
+
+url = base_url+urllib.quote_plus(query)
+r = requests.get(url)
+data = r.text
+soup = BeautifulSoup(data)
+
+# get all the search results on the first page
+for link in soup.find_all("a", class_="search-result-link"):
+    # get the links
+    page = link.get('href')
+    # get the titles
+    title = link.get_text()
+    # filter out old doc versions
+    if 'CD/' not in page and 'DOC' not in page:
+        print title
+        print alpine+page
